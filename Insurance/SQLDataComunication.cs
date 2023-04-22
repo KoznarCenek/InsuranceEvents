58f329b7cfcee378d9f8a8ebcfe6e6e5841a382c
Cenek Koznar <koznar.cenek@gmail.com>
2023-04-22 09:44:42 +0200

    Pripojení Aplikace na databázi

---
+++ Insurance/SQLDataComunication.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Reflection.PortableExecutable;
+
+namespace Insurance
+{
+    class SQLDataCommunication
+    {
+        public static void PripojSeNaDatabazi()
+        {
+            string connetionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Cenek\\Dropbox\\Csharp\\Insurance\\Insurance\\Pojistenci.mdf;Integrated Security=True";
+            string sql = "SELECT * FROM Pojistenci";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connetionString))
+                {
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    connection.Open();
+                    SqlDataReader dataReader = command.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        Object[] values = new Object[dataReader.FieldCount];
+                        int fieldCount = dataReader.GetValues(values);
+                        foreach (var value in values)
+                        {
+                            Console.Write(value + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    dataReader.Close();
+                    command.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can not open connection: " + ex.Message);
+            }
+        }
+    }
+}
+
