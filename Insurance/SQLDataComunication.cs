49e4ee73d3edc84ade71831be6390c0d6f108afc
Cenek Koznar <koznar.cenek@gmail.com>
2023-04-23 07:51:10 +0200

    Připojení do MS-Db za použití connection strungu za pomocí XML souboru.

--- Insurance/SQLDataComunication.cs
+++ Insurance/SQLDataComunication.cs
@@ -1,14 +1,43 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.Reflection.PortableExecutable;
+using System.Xml.Linq;
 
 namespace Insurance
 {
     class SQLDataCommunication
     {
+        public static string getconnetionString()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            Console.WriteLine(projectDirectory);
+            Console.WriteLine(workingDirectory);
+            string filepath = Path.Join(projectDirectory, "ConnectionString.xml");
+            string connetionString ="";
+            Console.WriteLine(connetionString);
+            try
+            {
+                var xml = XDocument.Load(filepath);
+
+                string DataSource = xml.Root.Element("DataSource").Value;
+               
+                string AttachDbFilename = xml.Root.Element("AttachDbFilename").Value;
+                    
+                string IntegratedSecurity = xml.Root.Element("IntegratedSecurity").Value;
+                    
+                connetionString = $"Data Source={DataSource};AttachDbFilename={AttachDbFilename};Integrated Security={IntegratedSecurity}";
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Nepodařilo se načíst XML soubor s connectionString");
+            }
+           return connetionString;
+        }
         public static void PripojSeNaDatabazi()
         {
-            string connetionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Cenek\\Dropbox\\Csharp\\Insurance\\Insurance\\Pojistenci.mdf;Integrated Security=True";
+            string connetionString = getconnetionString();
             string sql = "SELECT * FROM Pojistenci";
             try
             {
